df0798810f62ad15221874be8970f51d608cf1b8
Ignacio Rossi <rossi.ignacio@gmail.com>
2023-12-06 16:25:01 -0300

    Add local nacho configx

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,36 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+
+    on_attach(client,bufnr)
+  end,
+  capabilities = capabilities,
+}
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+-- lspconfig.rust_analyzer.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = {"rust"},
+--   root_dir = util.root_pattern("Cargo.toml"),
+--   settions = {
+--     ['rust-analyzer'] = {
+--       cargo = {
+--         allFeatures = true,
+--       }
+--     }
+--   }
+-- })
+
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,86 @@
+local plugins = {
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui-config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_initialized["dapui-config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui-config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {}
+    }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_,_)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "mypy",
+        "pyright",
+        "ruff",
+        "rust-analyzer",
+      }
+    }
+  }
+}
+return plugins

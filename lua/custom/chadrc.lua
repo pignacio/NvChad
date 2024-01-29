3e084dfd2f729751648277dd47dfafa223b3940e
Ignacio Rossi <pignacio@users.noreply.github.com>
2024-01-29 19:13:54 -0300

    Brighter greys

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,19 @@
 --@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+  theme = 'catppuccin',
+  changed_themes = {
+    catppuccin = {
+      base_30 = {
+        grey = "#888888",
+        grey_fg = "#999999",
+        grey_fg2 = "#aaaaaa"
+      },
+    },
+  },
+}
+
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
 return M
